3116d8ab501ebeb32f33e2612cc553c563204b5b
polymetal0 <pablo.garijo93@gmail.com>
2022-06-24 22:55:43 +0200

    Update PostProcessOutline.cs

--- Assets/Main_Profiles/PostProcessOutline.cs
+++ Assets/Main_Profiles/PostProcessOutline.cs
@@ -6,18 +6,12 @@ using UnityEngine.Rendering.PostProcessing;
 [PostProcess(typeof(PostProcessOutlineRenderer), PostProcessEvent.BeforeStack, "PGATR/Post Process Outline")]
 public sealed class PostProcessOutline : PostProcessEffectSettings
 {
-    [Tooltip("Number of pixels between samples that are tested for an edge. When this value is 1, tested samples are adjacent.")]
     public IntParameter scale = new IntParameter { value = 1 };
     public ColorParameter color = new ColorParameter { value = Color.black };
-    [Tooltip("Difference between depth values, scaled by the current depth, required to draw an edge.")]
     public FloatParameter depthThreshold = new FloatParameter { value = 1.5f };
-    [Range(0, 1), Tooltip("The value at which the dot product between the surface normal and the view direction will affect " +
-        "the depth threshold. This ensures that surfaces at right angles to the camera require a larger depth threshold to draw " +
-        "an edge, avoiding edges being drawn along slopes.")]
+    [Range(0, 1)]
     public FloatParameter depthNormalThreshold = new FloatParameter { value = 0.5f };
-    [Tooltip("Scale the strength of how much the depthNormalThreshold affects the depth threshold.")]
     public FloatParameter depthNormalThresholdScale = new FloatParameter { value = 7 };
-    [Range(0, 1), Tooltip("Larger values will require the difference between normals to be greater to draw an edge.")]
     public FloatParameter normalThreshold = new FloatParameter { value = 0.4f };    
 }
 
@@ -36,7 +30,6 @@ public sealed class PostProcessOutlineRenderer : PostProcessEffectRenderer<PostP
 
         Matrix4x4 clipToView = GL.GetGPUProjectionMatrix(context.camera.projectionMatrix, true).inverse;
         sheet.properties.SetMatrix("_ClipToView", clipToView);
-
         context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
     }
 }
